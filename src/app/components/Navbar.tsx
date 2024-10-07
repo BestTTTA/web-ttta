597d43026d0d4a12d656bbf012f9efefa56ef20e
BestTTTA <bst12546@gmail.com>
2024-10-07 14:48:50 +0700

    update color navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 
 const Navbar: FC = () => {
-  return <div className="fixed top-0 bg-sky-500 opacity-85 w-full">Navbar test1</div>;
+  return <div className="fixed top-0 bg-red-500 opacity-85 w-full">Navbar test1</div>;
 };
 
 export default Navbar;
