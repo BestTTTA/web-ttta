90c7ca0130669939c0cd51e4ec126ff5d8e9d283
Phakon77 <phakonsikhao@gmail.com>
2024-10-07 14:58:05 +0700

    update page4

---
+++ src/app/components/Page4.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+const Page4: FC = () => {
+  return <div>
+    <h1>The Tiger Team Academy</h1>
+    <p>Lorem ipsum dolor sit amet consectetur.Quis</p>
+    <p>sapiet sagittis at mattis ornare eros nec amet</p>
+    <p>aliquet. sit quis nullam viverra sed tempor</p>
+    <p>condimentum vivamus mattis</p>
+  </div>;
+};
+
+export default Page4;
